67244d1b5096b7edc1e3899ad7199807c244b09c
T-101 <T-101@PC250>
2016-01-22 21:41:04 -0600

    formulario

---
+++ proyecto_elementos/src/java/Controllers/ServletAutenticar.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author T-101
+ */
+public class ServletAutenticar extends HttpServlet {
+
+  
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       PrintWriter out= response.getWriter();
+    out.println("este es un servlet con el metodo post");
+    }
+}
